bb35805415de300b47d479301937fc2fc7302a7b
GoodnessJames <akomagoodness97@gmail.com>
2023-11-09 23:45:32 +0100

    Cleanup and setup web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+# Fabric file that deletes out-of-date archives.
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
-
+env.hosts = ["34.202.233.195", "100.26.216.182"]
+i
 
 def do_clean(number=0):
     """Delete out-of-date archives.
