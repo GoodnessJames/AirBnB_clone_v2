1477c461e4836ce1d6cff32d15f8111d6aff0889
GoodnessJames <akomagoodness97@gmail.com>
2023-11-09 21:27:18 +0100

    Setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ apt-get install -y nginx
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Hello World!" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
@@ -36,30 +36,3 @@ printf %s "server {
 }" > /etc/nginx/sites-available/default
 
 service nginx restart
-# Script to set up web servers for deployment of web_static
-
-# Install Nginx if not already installed
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-# Create necessary folders
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership to the ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-sudo sed -i '/^\tlocation \/ {$/a\
-\t\talias /data/web_static/current/;' /etc/nginx/sites-available/default
-
-# Restart Nginx
-sudo service nginx restart
-
-exit 0
