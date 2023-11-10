754e0bd15237c72b8fcfa48157ddf09272e12fd5
Elizabeth Ginika <elizabethginika9@gmail.com>
2023-11-09 18:06:19 -0800

    deploy web

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 Fabfile to distribute an archive to a web server.
 """
-=======
-# Fabric file to distribute an archive to a web server.
->>>>>>> cf79547f07a5aaaf592fba355822235780c8902e
 import os.path
 from fabric.api import env, put, run
 
 env.hosts = ["web1.goodnessjames.tech", "web2.goodnessjames.tech"]
 
+
 def do_deploy(archive_path):
     """
     Distributes an archive to a web server.
-
+    
     Args:
         archive_path (str): The path of the archive to distribute.
-    Return:
+    Returns:
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
@@ -37,7 +34,8 @@ def do_deploy(archive_path):
         return False
     if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed:
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed:
         return False
@@ -45,5 +43,6 @@ def do_deploy(archive_path):
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed:
         return False
+
     return True
 
